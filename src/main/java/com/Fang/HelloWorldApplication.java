fa587107b50976f79ba754687b4d28d8e02d1abd
Maxs <2363974355@qq.com>
2020-07-17 15:48:46 +0800

    test

---
+++ src/main/java/com/Fang/HelloWorldApplication.java
@@ -0,0 +1,16 @@
+package com.Fang;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * springboot主程序
+ */
+@SpringBootApplication
+public class HelloWorldApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HelloWorldApplication.class, args);
+    }
+}
